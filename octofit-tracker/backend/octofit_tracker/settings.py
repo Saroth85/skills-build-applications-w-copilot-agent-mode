6964eb1f9d0901be1dd90a09fe9e2a9b465e41e0
Rosario Barbagallo <39705101+Saroth85@users.noreply.github.com>
2025-04-08 21:54:37 +0000

    done

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-93j6jy%o*)yjz&-e8fhj+xm9zjz9$#8y0%l@i&9b598l1q&xl(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'legendary-zebra-vgj7jxqxj6jfw96-8000.app.github.dev']
 
 
 # Application definition
